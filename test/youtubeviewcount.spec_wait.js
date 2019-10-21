ba7fb94ee6d9efd23df3526a88f85e15afd17088
Fly Chang <leo424y@gmail.com>
2019-10-21 18:07:54 +0800

    modulize input parameter

--- test/youtubeviewcount.spec_wait.js
+++ test/youtubeviewcount.spec_wait.js
@@ -9,41 +9,49 @@ const screen = {
   height: 480
 };
 
-describe('youtube view count', function() {
-  this.timeout(30000)
-  let driver
-  let vars
-  beforeEach(async function() {
-    driver = await new Builder()
-    .forBrowser('firefox')
-    .setFirefoxOptions(new firefox.Options().headless().windowSize(screen))
-    .build()
-    vars = {}
-  })
-  afterEach(async function() {
-    await driver.quit();
-  })
-  it('youtube view count', async function() {
-    var yid = 'mspbVxNxO7M'
-    await driver.get("https://www.youtube.com/watch?v="+yid)
-    await driver.wait(until.elementLocated(By.css(".view-count")), 30000)
-    vars["vc"] = await driver.findElement(By.css(".view-count")).getText()
-    console.log(vars["vc"])
-    var timeInMs = Date.now();
-    let thenum = yid +', ' + retnum(vars["vc"]) + ', ' + timeInMs + '\n';
-    var fs = require('fs');
-
-    fs.appendFile(yid + '.txt', thenum, function (err) {
-        if (err)
-            console.log(err);
-        else
-            console.log('Write operation complete.');
-    });    
-  })
-})
+const items = ['mspbVxNxO7M', 'EfTH22VoEO0'];
+const copy = [];
 
 function retnum(str) { 
   var num = str.replace(/[^0-9]/g, ''); 
   return parseInt(num,10); 
 }
 
+function _Fn(val){
+  describe('youtube view count', function() {
+    this.timeout(30000)
+    let driver
+    let vars
+    beforeEach(async function() {
+      driver = await new Builder()
+      .forBrowser('firefox')
+      .setFirefoxOptions(new firefox.Options().headless().windowSize(screen))
+      .build()
+      vars = {}
+    })
+    afterEach(async function() {
+      await driver.quit();
+    })
+    it('youtube view count', async function() {
+      var yid = val
+      await driver.get("https://www.youtube.com/watch?v="+yid)
+      await driver.wait(until.elementLocated(By.css(".view-count")), 30000)
+      vars["vc"] = await driver.findElement(By.css(".view-count")).getText()
+      console.log(vars["vc"])
+      var timeInMs = Date.now();
+      let thenum = yid +', ' + retnum(vars["vc"]) + ', ' + timeInMs + '\n';
+      var fs = require('fs');
+  
+      fs.appendFile(yid + '.txt', thenum, function (err) {
+          if (err)
+              console.log(err);
+          else
+              console.log('Write operation complete.');
+      });    
+    })
+  })
+}
+
+items.forEach(function(item){
+  _Fn(item);
+});
